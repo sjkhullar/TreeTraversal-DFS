a51f0c8bd56386c9e08829b4cbfedf3ca3f5ca1d
geeks2champ <geeks2champ@gmail.com>
2016-02-20 18:33:52 +0530

    Tree traversal DFS using pre order, post order and inorder traversal

---
+++ TreeTraversal(dfs)/src/main/java/org/learn/Question/DFSTraversal.java
@@ -0,0 +1,32 @@
+package org.learn.Question;
+
+public class DFSTraversal {
+	
+	public static void preOrder(Node root) {
+		if (null == root) {
+			return;
+		}
+		System.out.printf("%d ", root.data);
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+	
+	public static void postOrder(Node root) {
+		if (null == root) {
+			return;
+		}
+		postOrder(root.left);		
+		postOrder(root.right);
+		System.out.printf("%d ", root.data);
+	}
+	
+	public static void inOrder(Node root) {
+		if (null == root)
+			return;
+		inOrder(root.left);
+		System.out.printf("%d ", root.data);
+		inOrder(root.right);
+	}
+	
+	
+}
